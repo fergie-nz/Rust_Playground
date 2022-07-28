53fa674ee919a413c460f33010a13585691ad8d1
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2022-07-29 09:34:03 +1200

    implementing lyric repitition

--- src/main.rs
+++ src/main.rs
@@ -34,9 +34,11 @@ fn main() {
             },
             2 => {
                 println!("you chose {fibonacci}");
+                fib();
             },
             3 => {
                 println!("you chose {christmas}");
+                lyrics()
             },
             4 => {
                 break;
@@ -68,4 +70,58 @@ fn temp() {
     let tfarenheight:i64 = (tcelsius*9/5)+32;
 
     println!("{tcelsius}C converted to farenheit is {tfarenheight}F");
+}
+
+fn fib() {
+    println!("Enter the n, and this will calculate the nth fibonacci number");
+
+    let mut number = String::new();
+
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line");
+    
+    let number: i64 = match number
+    .trim()
+    .parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("You must enter a number");
+            return;
+        }
+    };
+
+    let fibonacci = fibnum(number);
+    println!("the {number}th fibonacci number is {fibonacci}");
+    // println!("{fibonacci}")
+}
+
+fn fibnum(num:i64) -> i64 {
+    if num < 1 {
+        return 0;
+    } else if num == 1 {
+        return 1;
+    } else {
+        return {
+            fibnum(num-1) + fibnum(num-2)
+        };
+    }   
+}
+
+fn lyrics() {
+    for number in 1..12 {
+        let mut suffix = String::new()
+        if number = 1 {
+            suffix = "st"
+        } else if number = 2 {
+            suffix = "nd"
+        } else if number = 3 {
+            suffix = "rd"
+        } else {
+            suffix = "th"
+        }
+        println!("On the {number}{suffix} day of Christmas, my true love sent to me");
+        println!("a partridge and a pear tree")
+    }
+    println!("LIFTOFF!!!");
 }
